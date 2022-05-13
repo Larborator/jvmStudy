e75bd9dba216a6b783f8ec4df09b53f315c3281b
Larborator <466902955@qq.com>
2022-05-13 22:52:59 +0800

    Initial commit

---
+++ src/jvmStudy/ReferenceCountingGC.java
@@ -0,0 +1,25 @@
+package jvmStudy;
+
+public class ReferenceCountingGC {
+    public Object instance = null;
+    private static final int _1MB = 1024 * 1024;
+    private final byte[] bigSize = new byte[2 * _1MB];
+
+    public static void testGC() {
+        ReferenceCountingGC objcA = new ReferenceCountingGC();
+        ReferenceCountingGC objcB = new ReferenceCountingGC();
+        objcA.instance = objcB;
+        objcB.instance = objcA;
+
+        objcA = null;
+        objcB = null;
+
+        System.gc();
+        System.out.println(objcA);
+        System.out.println(objcB);
+    }
+
+    public static void main(String[] args) {
+        testGC();
+    }
+}
---
+++ src/leetCode/Solution7.java
@@ -0,0 +1,18 @@
+package leetCode;
+
+/**
+ * https://leetcode.cn/problems/reverse-integer/
+ *
+ * @author labuladuo
+ * @date 2022/5/12
+ */
+public class Solution7 {
+    public int reverse(int x) {
+        return x;
+    }
+
+    public static void main(String[] args) {
+        Solution7 s = new Solution7();
+        System.out.println(s.reverse(7));
+    }
+}
