c24abcc95e14f6c5122f7fde12063f0025db406e
labuladuo <labuladuo@douyu.tv>
2022-05-16 21:31:07 +0800

    note: *

---
+++ src/algorithm/leetcode/Solution14.java
@@ -0,0 +1,22 @@
+package algorithm.leetcode;
+
+/**
+ * https://leetcode.cn/problems/longest-common-prefix/
+ *
+ * @author labuladuo
+ * @date 2022/5/16
+ */
+public class Solution14 {
+    public String myFunction(String[] strs) {
+        for (int i = 0; i < strs.length; i++) {
+            System.out.println(strs[i]);
+            // todo
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Solution14 s = new Solution14();
+        System.out.println(s.myFunction(new String[]{"flower","flow","flight"}));
+    }
+}
