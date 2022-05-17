c8bc12d5d7d4f734fa5ced9b86ed4975b1bf9c2f
Larborator <466902955@qq.com>
2022-05-17 22:01:55 +0800

    note: if条件的得出需要数学素养

--- src/algorithm/leetcode/Solution14.java
+++ src/algorithm/leetcode/Solution14.java
@@ -1,5 +1,7 @@
 package algorithm.leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.cn/problems/longest-common-prefix/
  *
@@ -8,15 +10,27 @@ package algorithm.leetcode;
  */
 public class Solution14 {
     public String myFunction(String[] strs) {
-        for (int i = 0; i < strs.length; i++) {
-            System.out.println(strs[i]);
-            // todo
+        StringBuilder sb = new StringBuilder();
+        int len = strs.length == 0 ? 0 : Arrays.stream(strs).map(String::length).min(Integer::compareTo).get();
+        for (int i = 0; i < len; i++) {
+            boolean flag = true;
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[0].charAt(i) != strs[j].charAt(i)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                sb.append(strs[0].charAt(i));
+            } else {
+                break;
+            }
         }
-        return "";
+        return sb.toString();
     }
 
     public static void main(String[] args) {
         Solution14 s = new Solution14();
-        System.out.println(s.myFunction(new String[]{"flower","flow","flight"}));
+        System.out.println(s.myFunction(new String[]{"cir","car"}));
     }
 }
